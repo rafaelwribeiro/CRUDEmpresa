48f748cd7105f003a458f39f6ac31f22771d92ff
Rafael William Ribeiro <rwribeiro.php@gmail.com>
2022-12-24 04:12:12 -0300

    Implementando API

---
+++ CompaniesAPI/Api/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CompaniesAPI.Api.Controllers
+{
+
+    [ApiController]
+    [Route("/")]
+    public class HomeController : ControllerBase
+    {
+        public async Task<IActionResult> Home()
+        {
+            return Ok("teste");
+        }
+    }
+}
